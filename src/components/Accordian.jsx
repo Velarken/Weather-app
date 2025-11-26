d3d2f4b23b753a82f50af6bd0d759a1681684802
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2025-11-26 08:55:27 -0500

    updated header to contain toggle button and related function

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
 const Accordian = ({
-    
+    sectionTitle,
 }) => {
+
     const [isOpen,setIsOpen] = useState(false);
     const handleToggle = () => {
-
+        setIsOpen(!isOpen);
     }
+
     return (
         <div className='accordian-container'>
-            <h2>{}</h2>
+            <div className="sectionHeader">
+                <h2>{sectionTitle}</h2>
+                <button onClick={handleToggle}>{isOpen ? 'expand' : 'collapse'}</button>
+            </div>
             {isOpen 
                 ? <div className="accordian-data hidden">
                     
